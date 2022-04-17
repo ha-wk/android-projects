de0ce6e1ecfe53ccdcc821b2b07591f47da33263
ha-wk <ayushkr0369@gmail.com>
2022-04-17 16:56:19 +0530

    'commit'

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit=findViewById(R.id.edit);
+        text=findViewById(R.id.text);
+    }
+    public void get(View v)
+    {
+        String apikey="6d97d6ae35d2acd3fe64bd455da3a091";
+        String cityname=edit.getText().toString();
+        String url="https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=6d97d6ae35d2acd3fe64bd455da3a091";
+        RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject object = response.getJSONObject("main");
+                    String temperature = object.getString("temp");
+                    Double temp=Double.parseDouble(temperature)-273.15;
+                    text.setText("Temperature is "+temp.toString().substring(0,2)+" degree C");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,"please enter valid city name",Toast.LENGTH_SHORT).show();
+            }
+
+    });
+        queue.add(request);
+
+    }
+}
